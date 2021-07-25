4ffc5ff95b2618444ffcf7de1632cfb4a0da25ce
enki.y <enki.y@boringdao.com>
2021-07-25 22:27:44 +0800

    local compile

---
+++ tcp2/target/debug/deps/tcp2-98572016705ac62b.d
@@ -0,0 +1,5 @@
+/home/codes/Rust_Course-1/tcp2/target/debug/deps/tcp2-98572016705ac62b: src/main.rs
+
+/home/codes/Rust_Course-1/tcp2/target/debug/deps/tcp2-98572016705ac62b.d: src/main.rs
+
+src/main.rs:
---
+++ tcp2/target/debug/tcp2.d
@@ -0,0 +1,1 @@
+/home/codes/Rust_Course-1/tcp2/target/debug/tcp2: /home/codes/Rust_Course-1/tcp2/src/main.rs
